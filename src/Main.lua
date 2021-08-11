36d05d9e02dd0777a6b83e05e2b395464e9874b7
sid-shakthivel <sid.shakthivel@gmail.com>
2021-08-11 19:12:39 +0100

    Added Comments

--- src/Main.lua
+++ src/Main.lua
@@ -8,12 +8,15 @@ local CurrentLine = File:read("*line")
 while CurrentLine ~= nil do
     Program = Program .. "\n" .. CurrentLine
     CurrentLine = File:read("*line")
+    if (CurrentLine ~= nil  and CurrentLine:sub(1, 1) == '#') then
+        CurrentLine = File:read("*line")
+    end
 end
 
 File:close()
 
---Interpreter = CInterpreter:new(Program)
---Interpreter:Execute()
+Interpreter = CInterpreter:new(Program)
+Interpreter:Execute()
 
-Lexer = CLexer:new(Program)
-Lexer:InvertTokens()
+--Lexer = CLexer:new(Program)
+--Lexer:InvertTokens()
