f5dca7da0325b7e2157500195c5e7ceb6e934508
sid-shakthivel <sid.shakthivel@gmail.com>
2021-08-13 10:40:25 +0100

    Added File Checking

--- src/Main.lua
+++ src/Main.lua
@@ -1,7 +1,17 @@
 local CInterpreter = require("src.Interpreter")[1]
-local CLexer = require("src.Lexer")[1]
+local Error = require("src.Error")
 
-local File = io.open("./programs/program1.txt", "r")
+local FileName = arg[1]
+
+if (FileName == nil) then
+    Error:Error("FILE ERROR: FILE NOT GIVEN")
+elseif (FileName:sub(#FileName-3, #FileName) ~= ".txt") then
+    Error:Error("FILE ERROR: FILE MUST BE A .TXT")
+elseif (assert(io.open(FileName, "r")) == false) then
+    Error:Error("FILE ERROR: CANNOT OPEN FILE " .. FileName)
+end
+
+local File = io.open(FileName, "r")
 local Program = ""
 local CurrentLine = File:read("*line")
 
@@ -17,26 +27,3 @@ File:close()
 
 Interpreter = CInterpreter:new(Program)
 Interpreter:Execute()
-
---Lexer = CLexer:new(Program)
---Lexer:InvertTokens()
---print(Lexer:GetNextToken().Value)
---print(Lexer:GetNextToken().Value)
---print(Lexer:GetNextToken().Value)
---print(Lexer:GetNextToken().Value)
---print(Lexer:GetNextToken().Value)
---print(Lexer:GetNextToken().Value)
---print(Lexer:GetNextToken().Value)
---print(Lexer:GetNextToken().Value)
---print(Lexer:GetNextToken().Value)
---print(Lexer:GetNextToken().Value)
---print(Lexer:GetNextToken().Value)
---print(Lexer:GetNextToken().Value)
---print(Lexer:GetNextToken().Value)
---print(Lexer:GetNextToken().Value)
---print(Lexer:GetNextToken().Value)
---print(Lexer:GetNextToken().Value)
---print(Lexer:GetNextToken().Value)
---print(Lexer:GetNextToken().Value)
---print(Lexer:GetNextToken().Value)
---print(Lexer:GetNextToken().Value)
